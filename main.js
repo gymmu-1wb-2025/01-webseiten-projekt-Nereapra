cf5afa3ac3bb63f4136ad068ee9206dea3f59b92
Nerea Prado <e273623@edu.sbl.ch>
2025-12-16 19:18:50 +0000

    16.12.25

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let counter = 0
 
 function createNewElement () {
-let newElement = document createElement ("p")
+let newElement = document.createElement ("p");
 
 counter = counter + 1;
 
-newElement textContent = "Element" + counter;
+newElement.textContent = "Element" + counter;
 
 let mainElement = document.querySelector("main");
 mainElement.appendChild(newElement);
